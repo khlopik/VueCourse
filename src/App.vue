891dd508af373b4f51d74fdb80079f829a8e0346
Oleksii Khlopotov <khlopik@gmail.com>
2017-11-27 17:38:42 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <app-servers></app-servers>
+</template>
+
+<script>
+  import Header from './components/Shared/Header.vue';
+  import Footer from './components/Shared/Footer.vue';
+
+  export default {
+    components: {
+      'app-header': Header,
+      'app-servers':
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
